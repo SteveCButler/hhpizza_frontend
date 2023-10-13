19a9e2e4b3bc8eb066e204f0b4224f23161462e4
Steve Butler <butlersteve.c@gmail.com>
2023-10-12 19:03:40 -0500

    dynamic route for Item/orderId, Item form submit fixed

--- components/CreateOrderForm.js
+++ components/CreateOrderForm.js
@@ -15,7 +15,7 @@ const initialState = {
 };
 
 export default function CreateOrderForm() {
-  const [formData, setFormData] = useState({ initialState });
+  const [formData, setFormData] = useState(initialState);
   const { user } = useAuth();
   const router = useRouter();
 
